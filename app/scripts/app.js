d999bab21bea766fd78010ce89a44906de420691
Prasad Rajandran <prasadrajandran@gmail.com>
2014-10-01 10:30:15 -0700

    Generate Yeoman angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name afredApp
+ * @description
+ * # afredApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('afredApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name afredApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the afredApp
+ */
+angular.module('afredApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
