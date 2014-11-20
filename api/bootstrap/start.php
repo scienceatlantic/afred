f8bf21164c9b37ae4bf1e759e603ea03ceebf1c7
Prasad Rajandran <prasadrajandran@gmail.com>
2014-11-20 17:29:24 -0400

    Add local and production environments to laravel

--- api/bootstrap/start.php
+++ api/bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('ubuntu'),
+        'production' => array('s7-toronto')
 
 ));
 
