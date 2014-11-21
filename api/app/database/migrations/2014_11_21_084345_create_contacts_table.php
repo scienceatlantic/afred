2191a63205ed21f066a4ad7c7ed03a6068422900
Prasad Rajandran <prasadrajandran@gmail.com>
2014-11-21 04:53:45 -0400

    Add contacts migration

---
+++ api/app/database/migrations/2014_11_21_084345_create_contacts_table.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration {
+    public function up()
+    {
+        Schema::create('contacts', function($table) {
+            $table->increments('id');
+            $table->integer('facility_id')->unsigned();
+            $table->foreign('facility_id')->references('id')->on('facilities')->onDelete('cascade');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email', 254);
+            $table->string('telephone', 10);
+            $table->string('position', 50)->nullable();
+            $table->string('department', 100)->nullable();
+            $table->boolean('editor');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop('contacts');
+    }
+}
