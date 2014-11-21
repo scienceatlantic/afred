b4a7e095438c6f2c2e22d2a951dcd51dfc399db8
Prasad Rajandran <prasadrajandran@gmail.com>
2014-11-20 20:21:45 -0400

    Update local laravel config file

--- api/.env.local.php
+++ api/.env.local.php
@@ -1,7 +1,20 @@
 <?php
 return array(
-    'DATABASE_HOST' => 'localhost',
-    'DATABASE_NAME' => 'afred',
-    'DATABASE_USERNAME' => 'root',
-    'DATABASE_PASSWORD' => 'root'
+    'APP_DEBUG'             => true,
+    'APP_KEY'               => 'KAmQyv8o2AKPhvlrgpxGhfq5cwtHZr01',
+  
+    'DATABASE_HOST'         => 'localhost',
+    'DATABASE_NAME'         => 'afred',
+    'DATABASE_USERNAME'     => 'root',
+    'DATABASE_PASSWORD'     => 'root',
+    
+    'MAIL_DRIVER'           => 'smtp',
+    'MAIL_HOST'             => 'smtp.mailgun.org',
+    'MAIL_PORT'             => '587',
+    'MAIL_FROM_ADDRESS'     => null,
+    'MAIL_FROM_NAME'        => null,
+    'MAIL_FROM_ENCRYPTION'  => 'tls',
+    'MAIL_USERNAME'         => null,
+    'MAIL_PASSWORD'         => null,
+    'MAIL_PRETEND'          => false
 );
