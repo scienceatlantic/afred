dfb96019cf908e6b694b783c9e07e99d33e66ebb
Prasad Rajandran <prasadrajandran@gmail.com>
2014-11-21 04:32:26 -0400

    Fix indentation in the facilities migration file

--- api/app/database/migrations/2014_11_21_072113_create_faculties_table.php
+++ api/app/database/migrations/2014_11_21_072113_create_faculties_table.php
@@ -1,38 +1,25 @@
 <?php
-
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateFacultiesTable extends Migration {
+    public function up()
+    {
+        Schema::create('facilities', function($table) {
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->string('institution', 50);
+            $table->string('city', 50);
+            $table->string('province', 50);
+            $table->string('website', 2083)->nullable(); //IE's maximum
+            $table->text('description');
+            $table->text('additional_info')->nullable();
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('facilities', function($table) {
-                    $table->increments('id');
-                    $table->string('name', 50);
-                    $table->string('institution', 50);
-                    $table->string('city', 50);
-                    $table->string('province', 50);
-                    $table->string('website', 2083)->nullable(); //IE's maximum
-                    $table->text('description');
-                    $table->text('additional_info')->nullable();
-                    $table->timestamps();
-                });
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('facilities');
-	}
-
+    public function down()
+    {
+        Schema::drop('facilities');
+    }
 }
