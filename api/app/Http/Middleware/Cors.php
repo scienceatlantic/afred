f5800ae41ec2b2b1a5c479ab259b3bf79bbc0187
Prasad Rajandran <prasadrajandran@gmail.com>
2015-11-16 23:37:31 -0400

    Update Cors to allow 'Content-Type'

--- api/app/Http/Middleware/Cors.php
+++ api/app/Http/Middleware/Cors.php
@@ -19,6 +19,7 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type');
     }
 }
