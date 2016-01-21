cdb28ab7aa981b9293304ceacb98d29dc4f9d648
Prasad Rajandran <prasadrajandran@gmail.com>
2016-01-21 15:07:21 -0400

    Bug fix
    Renamed frhId to frId

--- api/app/Facility.php
+++ api/app/Facility.php
@@ -23,7 +23,7 @@ class Facility extends Model
      * @var array
      */
     protected $fillable = [
-        'facilityRevisionHistoryId',
+        'facilityRepositoryId',
         'organizationId',
         'provinceId',
         'name',
