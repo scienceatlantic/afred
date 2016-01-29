56160fda5bfdd3659b46879a2bbacad6b7f4393f
Prasad Rajandran <prasadrajandran@gmail.com>
2016-01-29 14:42:20 -0400

    Update App directives
    To accommodate 'sectors' and 'disciplines'

--- app/app/scripts/directives/af-field.js
+++ app/app/scripts/directives/af-field.js
@@ -12,21 +12,33 @@ angular.module('afredApp').directive('afField',
       transclude: true,
       templateUrl: 'views/directives/af-field.html',
       scope: {
-        label: '@afFieldLabel'
+        label: '@afFieldLabel',
+        nameOverride: '@afFieldName',
+        typeOverride: '@afFieldType'
       },
-      link: function($scope, element, attrs, form) {      
-        var field = element.find('input, select, textarea');
-        $scope.name = field.attr('name');
-        $scope.id = field.attr('id');
-        $scope.form = form;
+      link: function($scope, element, attrs, form) { 
+        if ($scope.nameOverride) {
+          $scope.name = $scope.nameOverride;
+          
+          $scope.template = {
+            isRadio: $scope.typeOverride === 'radio',
+            isCheckbox: $scope.typeOverride === 'checkbox'
+          };
+        } else {
+          var field = element.find('input, select, textarea');
+          $scope.name = field.attr('name');
+          $scope.id = field.attr('id');
+
+          $scope.template = {
+            isInput: field.prop('tagName') === 'INPUT',
+            isSelect: field.prop('tagName') === 'SELECT',
+            isTextarea: field.prop('tagName') === 'TEXTAREA',
+            isRadio: field.attr('type') === 'radio',
+            isCheckbox: field.attr('type') === 'checkbox'
+          };                   
+        }
         
-        $scope.template = {
-          isInput: field.prop('tagName') === 'INPUT',
-          isSelect: field.prop('tagName') === 'SELECT',
-          isTextarea: field.prop('tagName') === 'TEXTAREA',
-          isRadio: field.attr('type') === 'radio',
-          requiredField: field.attr('required')
-        };
+        $scope.form = form; 
       }
     };
   }
