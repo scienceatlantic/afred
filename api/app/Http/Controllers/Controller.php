e38d08249bb47b864f30ffe54a9f4243ded8cfd3
Prasad Rajandran <prasadrajandran@gmail.com>
2016-01-29 14:36:57 -0400

    Update API Base Controller
    - Add pagination capabilities.

--- api/app/Http/Controllers/Controller.php
+++ api/app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Http\Request;
 
 // Misc.
 use Carbon\Carbon;
@@ -15,6 +16,14 @@ use Log;
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    protected $_paginate;
+    protected $_itemsPerPage;
+    
+    function __construct(Request $request) {
+        $this->_paginate = $request->input('paginate', true);
+        $this->_itemsPerPage = $request->input('itemsPerPage', 15);        
+    }
     
     protected function _expandModelRelationships($request,
                                                  $model,
