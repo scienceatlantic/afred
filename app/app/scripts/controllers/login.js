7d83d780c6a99e2a87478953b205ac8bd36b7dce
Prasad Rajandran <prasadrajandran@gmail.com>
2016-02-18 23:01:02 -0400

    Update App Login page

--- app/app/scripts/controllers/login.js
+++ app/app/scripts/controllers/login.js
@@ -7,12 +7,24 @@ angular.module('afredApp').controller('LoginController',
      * Functions.
      * --------------------------------------------------------------------- */
   
+    /**
+     * Submits the login data to the API.
+     */
     $scope.submit = function() {
-      $scope._auth.login($scope.credentials).then(function(response) {
+      // Set loading flag to true.
+      $scope.loading.login = true;
+      
+      $scope.auth = $scope._auth.login($scope.credentials).then(function(response) {
+        // If login was successful, save the authenticated user's details
+        // and redirect the user to the dashboard.
         $scope._auth.user = response.data;
         $scope._state.go('admin.dashboard');
       }, function() {
+        console.log($scope.auth);
         $scope.credentials.invalid = true;
+        
+        // Set loading flag to false.
+        $scope.loading.login = false;
       });
     };
     
@@ -20,10 +32,20 @@ angular.module('afredApp').controller('LoginController',
      * Initialisation code.
      * --------------------------------------------------------------------- */
     
+    /**
+     * Stores the form data that will be passed to the API.
+     * 
+     * @type {object}
+     */
     $scope.credentials = {
       email: null,
       password: null,
-      invalid: false
+      invalid: false // Used to display an error message if login failed.
+    };
+    
+    // Loading flags. 
+    $scope.loading = {
+      login: false
     };
   }
 ]);
