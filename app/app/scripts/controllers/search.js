98eb0291c98dc06977deaf79919ebb1fd06fa919
Prasad Rajandran <prasadrajandran@gmail.com>
2016-02-23 00:01:10 -0400

    Update app search

--- app/app/scripts/controllers/search.js
+++ app/app/scripts/controllers/search.js
@@ -4,68 +4,181 @@ angular.module('afredApp').controller('SearchController',
   ['$scope',
    'provinceResource',
    'organizationResource',
+   'disciplineResource',
+   'sectorResource',
+   'searchResource',
    '$uibModal',
    function($scope,
             provinceResource,
             organizationResource,
+            disciplineResource,
+            sectorResource,
+            searchResource,
             $uibModal) {
     /* ---------------------------------------------------------------------
      * Functions.
      * --------------------------------------------------------------------- */
     
-    /**
-     * Performs a search request.
-     */
-    $scope.searchFn = function() {
-      // Search only if the query is not empty
-      if ($scope.search.q) {
-        $scope._state.go('search.q', {q: $scope.search.q});
-      } else { // Otherwise return to the main search page
-        $scope._state.go('search');
+    $scope.search = {
+      query: {
+        q: null,
+        'disciplineId[]': null,
+        'sectorId[]': null,
+        'organizationId[]': null,
+        'provinceId[]': null,
+        type: 'facility',
+      },
+      
+      advanced: {
+        radios: {
+          provinces: true,
+          organizations: true,
+          disciplines: true,
+          sectors: true,
+          
+          update: function() {
+            if ($scope.search.advanced.radios.provinces) {
+              $scope.search.query['provinceId[]'] = [];
+            }
+            
+            if ($scope.search.advanced.radios.organizations) {
+              $scope.search.query['organizationId[]'] = [];
+            }
+            
+            if ($scope.search.advanced.radios.disciplines) {
+              $scope.search.query['disciplineId[]'] = [];
+            }
+            
+            if ($scope.search.advanced.radios.sectors) {
+              $scope.search.query['sectorId[]'] = [];
+            }
+          }
+        }
+      },
+      
+      results: {},
+      
+      disciplines: [],
+      
+      sectors: [],
+      
+      organizations: [],
+      
+      provinces: [],
+      
+      goToResultsPage: function(showAll) {
+        if (!showAll) {
+          // Search only if the query is not empty
+          if ($scope.search.query.q) {
+            $scope._state.go('search.q', $scope.search.query);
+          } else { // Otherwise return to the main search page
+            $scope._state.go('search');
+          }   
+        } else {
+          $scope._state.go('search.all', $scope.search.query);
+        }
+    
+      },
+      
+      parseParams: function() {        
+        $scope.search.query = {
+          q: $scope._stateParams.q,
+          type: $scope._stateParams.type == 'facility' ?
+            'facility' : 'equipment',
+          'provinceId[]':
+            $scope.search.toInt($scope._stateParams['provinceId[]']),
+          'organizationId[]':
+            $scope.search.toInt($scope._stateParams['organizationId[]']),
+          'disciplineId[]':
+            $scope.search.toInt($scope._stateParams['disciplineId[]']),
+          'sectorId[]':
+            $scope.search.toInt($scope._stateParams['sectorId[]'])
+        };
+        
+        $scope.search.advanced.radios.provinces =
+          $scope.search.query['provinceId[]'].length == 0;
+        
+        $scope.search.advanced.radios.organizations =
+          $scope.search.query['organizationId[]'].length == 0;
+          
+        $scope.search.advanced.radios.disciplines =
+          $scope.search.query['disciplineId[]'].length == 0;
+          
+        $scope.search.advanced.radios.sectors =
+          $scope.search.query['sectorId[]'].length == 0;
+      },
+      
+      toInt: function(arr) {
+        var values = [];
+        angular.forEach(arr, function(v, k) {
+          try {
+            values.push(parseInt(v));
+          } catch(e) {
+            // Do nothing.
+          }
+        });
+        return values;
+      },
+      
+      getResults: function() {
+        $scope.search.results = searchResource.query($scope.search.query);
+      },
+      
+      contactSpringboard: function() {
+        var modalInstance = $uibModal.open({
+          templateUrl: 'views/modals/contact-us.html',
+          controller: 'ContactUsModalController'
+        });
+        
+        modalInstance.dummy = 'dummy';        
+      },
+      
+      getDisciplines: function() {
+        $scope.search.disciplines = disciplineResource.queryNoPaginate();
+      },
+      
+      getSectors: function() {
+        $scope.search.sectors = sectorResource.queryNoPaginate();   
+      },
+      
+      getOrganizations: function() {
+        $scope.search.organizations = organizationResource.queryNoPaginate(null,
+          function() {
+            for (var i = 0; i < $scope.search.organizations.length; i++) {
+              if ($scope.search.organizations[i].name == 'N/A') {
+                $scope.search.organizations.splice(i, 1);
+                break;
+              }
+            }
+          }
+        );          
+      },
+      
+      getProvinces: function() {
+        $scope.search.provinces = provinceResource.queryNoPaginate(null,
+          function () {
+            for (var i = 0; i < $scope.search.provinces.length; i++) {
+              if ($scope.search.provinces[i].name == 'N/A') {
+                $scope.search.provinces.splice(i, 1);
+                break;
+              }
+            }
+          }
+        );        
+      },
+      
+      initialise: function() {
+        $scope.search.getDisciplines();
+        $scope.search.getSectors();
+        $scope.search.getOrganizations();
+        $scope.search.getProvinces();
       }
     };
-    
-    /**
-     * Instantiates a modal that allows the user to send
-     * a message to Springboard Atlantic.
-     */
-    $scope.contactUs = function() {
-      var modalInstance = $uibModal.open({
-        templateUrl: 'views/modals/contact-us.html',
-        controller: 'ContactUsModalController'
-      });
-      
-      modalInstance.dummy = 'dummy';
-    };
-    
-    $scope.getProvinces = function() {
-      $scope.provinces = provinceResource.queryNoPaginate(null, function() {
-        $scope.provinces.unshift({ id: -1, name: 'All' });
-      });
-    };
-    
-    $scope.getOrganizations = function() {
-      $scope.organizations = organizationResource.queryNoPaginate(null,
-        function() {
-          $scope.organizations.unshift({ id: -1, name: 'All' });
-        }
-      );
-    }
     
     /* ---------------------------------------------------------------------
      * Initialisation code.
      * --------------------------------------------------------------------- */
     
-    // This will store the search queries.
-    $scope.search = {
-      q: null,
-      listBy: 'equipment',
-    };
-    
-    $scope.provinces = null;
-    $scope.organizations = null;
-    
-    $scope.getProvinces();
-    $scope.getOrganizations();
+    $scope.search.initialise();
   }
 ]);
--- app/app/scripts/controllers/search.results.js
+++ app/app/scripts/controllers/search.results.js
@@ -2,54 +2,17 @@
 
 angular.module('afredApp').controller('SearchResultsController',
   ['$scope',
-   'searchResource',
-  function($scope,
-           searchResource) {
-    /* ---------------------------------------------------------------------
-     * Functions.
-     * --------------------------------------------------------------------- */
-    
-    /**
-     * Query the database.
-     */
-    $scope.getSearchResults = function(q) {
-      searchResource.query({q: q}, function(data) {
-        $scope.results.data = data;
-        $scope.loading.searchResults = false;
-      });
-    };
-    
-    /**
-     * Redirects the user to the equipment's page.
-     */
-    $scope.showEquipmentPage = function(facilityId, equipmentId) {
-      $scope._state.go('equipment.show', {
-        facilityId: facilityId,
-        equipmentId: equipmentId
-      });
-    };
-    
+  function($scope) {    
     /* ---------------------------------------------------------------------
      * Initialisation code.
      * --------------------------------------------------------------------- */
     
-    // This object will store the results.
-    $scope.results = {
-      data: null
-    };
-    
-    // AJAX loading flag.
-    $scope.loading = {
-      searchResults: true
-    };
-    
     // Since the search query is appended to the URL, users might potentially
-    // bypass the search bar altogether. If they do that, we'll just copy
-    // the query from the URL into the search bar.
-    $scope.search.q = ($scope._state.is('search.q') ?
-      $scope._stateParams.q : null);
-    
+    // bypass the search bar altogether. Regardless of whether or not they do
+    // that, we'll just copy the query from the URL into the search bar.
+    $scope.search.parseParams();
+   
     // Run the search.
-    $scope.getSearchResults($scope.search.q);
+    $scope.search.getResults();
   }
 ]);
