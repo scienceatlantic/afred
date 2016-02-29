eae239010f729d9f136cbfbf50d640a040ae2787
Prasad Rajandran <prasadrajandran@gmail.com>
2016-02-29 16:56:43 -0400

    Update services.js
    Minor enhancements to confirmModal

--- app/app/scripts/services.js
+++ app/app/scripts/services.js
@@ -3,13 +3,13 @@
 angular.module('afredApp').service('confirmModal', [
   '$uibModal',
   function($uibModal) {    
-    this.open = function(body) {
+    this.open = function(template) {
       return $uibModal.open({
         templateUrl: 'views/modals/confirm.html',
         controller: ['$scope', '$uibModalInstance',
           function($scope, $uibModalInstance) {
             $scope.modal = $uibModalInstance;
-            $scope.body = body;
+            $scope.template = 'views/includes/modal-' + template + '.html';
           }
         ]
       });        
