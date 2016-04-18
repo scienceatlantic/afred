babb2fae161915e0d99c354b0a98f4d786e230bc
Prasad Rajandran <prasadrajandran@gmail.com>
2016-04-17 22:07:45 -0300

    Update resources.js
    - Improve formatting
    - Add missing code
    - Add WordPress resource

--- app/app/scripts/resources.js
+++ app/app/scripts/resources.js
@@ -1,103 +1,263 @@
 'use strict';
 
-angular.module('afredApp').factory('facilityResource', ['$rootScope',
-  '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address + '/facilities/:facilityId',
+angular.module('afredApp').factory('facilityResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/facilities/:facilityId',
       {
         facilityId: '@id'
       },
       {
-        query: { isArray: false },
-        update: { method: 'PUT' }
+        query: {
+          method: 'GET',
+          isArray: false
+        },
+        update: {
+          method: 'PUT'
+        }
       }
     );
   }
 ]);
 
 angular.module('afredApp').factory('facilityRepositoryResource',
-  ['$rootScope', '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address +
-      '/facility-repository/:facilityRepositoryId',
-        {
-          facilityRepositoryId: '@id'
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/facility-repository/:facilityRepositoryId',
+      {
+        facilityRepositoryId: '@id'
+      },
+      {
+        query: {
+          method: 'GET',
+          isArray: false
         },
-        {
-          query: { isArray: false },
-          submit: { method: 'POST', params: { state: 'PENDING_APPROVAL' }},
-          approve: { method: 'PUT', params: { state: 'PUBLISHED' }},
-          reject: { method: 'PUT', params: { state: 'REJECTED' }},
-          submitEdit: { method: 'PUT', params: { state:
-            'PENDING_EDIT_APPROVAL' }},
-          approveEdit: { method: 'PUT', params: { state: 'PUBLISHED_EDIT' }},
-          rejectEdit: { method: 'PUT', params: { state: 'REJECTED_EDIT' }},
-          
-          queryTokens: { url: $rootScope._config.api.address +
-            '/facilities', method: 'GET', isArray: false },
-          queryTokensNoPaginate: { url: $rootScope._config.api.address +
-            '/facility-update-links', method: 'GET', isArray: true,
-            params: { paginate: 0 }},
-          generateToken: { url: $rootScope._config.api.address +
-            '/facility-update-links/generate-token', method: 'POST' },
-          verifyToken: { url: $rootScope._config.api.address +
-            '/facility-repository/verify-token', method: 'POST' }
+        submit: {
+          method: 'POST',
+          params: {
+            state: 'PENDING_APPROVAL'
+          }
+        },
+        approve: {
+          method: 'PUT',
+          params: {
+            state: 'PUBLISHED'
+          }
+        },
+        reject: {
+          method: 'PUT',
+          params: {
+            state: 'REJECTED'
+          }
+        },
+        submitEdit: {
+          method: 'PUT',
+          params: {
+            state: 'PENDING_EDIT_APPROVAL'
+          }
+        },
+        approveEdit: {
+          method: 'PUT',
+          params: {
+            state: 'PUBLISHED_EDIT'
+          }
+        },
+        rejectEdit: {
+          method: 'PUT',
+          params: {
+            state: 'REJECTED_EDIT'
+          }
+        },
+        queryTokens: {
+          url: root + '/facilities',
+          method: 'GET',
+          isArray: false
+        },
+        queryTokensNoPaginate: {
+          url: root + '/facilities',
+          method: 'GET',
+          isArray: true,
+          params: {
+            paginate: 0
+          }
+        },
+        generateToken: {
+          url: root + '/facility-update-links/generate-token',
+          method: 'POST'
+        },
+        verifyToken: {
+          url: root + '/facility-repository/verify-token',
+          method: 'POST'
         }
+      }
     );
   }
 ]);
 
-angular.module('afredApp').factory('searchResource', ['$rootScope',
-  '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address + '/search', null, {
-      query: { isArray: false }
+angular.module('afredApp').factory('searchResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/search', null, {
+      query: {
+        method: 'GET',
+        isArray: false
+      }
     });
   }
 ]);
 
-angular.module('afredApp').factory('organizationResource', ['$rootScope',
-  '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address +
-      '/organizations/:organizationId',
-      {},
+angular.module('afredApp').factory('organizationResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/organizations/:organizationId',
       {
-        query: { isArray: false },
-        queryNoPaginate: { isArray: true, params: { paginate: 0 }}
+        organizationId: '@id'
+      },
+      {
+        query: {
+          method: 'GET',
+          isArray: false
+        },
+        queryNoPaginate: {
+          method: 'GET',
+          isArray: true,
+          params: {
+            paginate: 0
+          }
+        }
       }
     );
   }
 ]);
 
-angular.module('afredApp').factory('provinceResource', ['$rootScope',
-  '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address + '/provinces/:provinceId',
-      {},
+angular.module('afredApp').factory('provinceResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/provinces/:provinceId',
       {
-        query: { isArray: false },
-        queryNoPaginate: { isArray: true, params: { paginate: 0 }}
+        provinceId: '@id'
+      },
+      {
+        query: {
+          method: 'GET',
+          isArray: false
+        },
+        queryNoPaginate: {
+          method: 'GET',
+          isArray: true,
+          params: {
+            paginate: 0
+          }
+        }
       }
     );
   }
 ]);
 
-angular.module('afredApp').factory('disciplineResource', ['$rootScope',
-  '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address +
-      '/disciplines/:disciplineId',
-      {},
+angular.module('afredApp').factory('disciplineResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/disciplines/:disciplineId',
       {
-        query: { isArray: false },
-        queryNoPaginate: { isArray: true, params: { paginate: 0 }}
+        disciplineId: '@id'  
+      },
+      {
+        query: {
+          method: 'GET',
+          isArray: false
+        },
+        queryNoPaginate: {
+          isArray: true,
+          params: {
+            paginate: 0
+          }
+        }
       }
     );
   }
 ]);
 
-angular.module('afredApp').factory('sectorResource', ['$rootScope',
-  '$resource', function($rootScope, $resource) {
-    return $resource($rootScope._config.api.address + '/sectors/:sectorId',
-      {},
+angular.module('afredApp').factory('sectorResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.api.address;
+    
+    return $resource(root + '/sectors/:sectorId',
       {
-        query: { isArray: false },
-        queryNoPaginate: { isArray: true, params: { paginate: 0 }}
+        sectorId: '@id'
+      },
+      {
+        query: {
+          method: 'GET',
+          isArray: false
+        },
+        queryNoPaginate: {
+          method: 'GET',
+          isArray: true,
+          params: {
+            paginate: 0
+          }
+        }
+      }
+    );
+  }
+]);
+
+angular.module('afredApp').factory('wpResource',
+  ['$rootScope',
+   '$resource',
+   function($rootScope,
+            $resource) {
+    var root = $rootScope._config.wp.address;
+    
+    var headers = {
+      'Authorization': 'Basic '
+        + btoa($rootScope._config.wp.appAuth.username + ':'
+        + $rootScope._config.wp.appAuth.password)
+    };
+    
+    return $resource(root, null,
+      {
+        queryPages: {
+          url: root + '/pages',
+          method: 'GET',
+          isArray: true,
+          headers,
+          withCredentials: false
+        },
+        getPage: {
+          url: root + '/pages/:pageId',
+          method: 'GET',
+          isArray: false,
+          headers,
+          withCredentials: false
+        }
       }
     );
   }
