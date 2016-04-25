1171fc89b3d8f80c6e3a793233bfbcee18c5a298
Prasad Rajandran <prasadrajandran@gmail.com>
2016-04-25 12:17:26 -0300

    Update organization schema (name should be unique)

--- api/database/migrations/2015_11_09_002043_create_organizations_table.php
+++ api/database/migrations/2015_11_09_002043_create_organizations_table.php
@@ -14,7 +14,8 @@ class CreateOrganizationsTable extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 150);
+            $table->string('name', 150)
+                ->unique();
             $table->boolean('isHidden')
                 ->default(false);
             $table->datetime('dateAdded');
