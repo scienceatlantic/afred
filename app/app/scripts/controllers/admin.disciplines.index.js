b7bba48b4719ca802af4946156b99d4b7fa6b732
Prasad Rajandran <prasadrajandran@gmail.com>
2016-05-19 12:52:01 -0300

    Update formatting

--- app/app/scripts/controllers/admin.disciplines.index.js
+++ app/app/scripts/controllers/admin.disciplines.index.js
@@ -1,13 +1,11 @@
 'use strict';
-
-angular.module('afredApp').controller('AdminDisciplinesIndexController', [
-  '$scope',
-  function($scope) {    
-    /* ---------------------------------------------------------------------
-     * Initialisation code.
+angular.module('afredApp').controller('AdminDisciplinesIndexController', [
+  '$scope',
+  function($scope) {    
+    /* ---------------------------------------------------------------------
+     * Initialisation code.
      * --------------------------------------------------------------------- */
-    
-    $scope.disciplines.parseParams();
-    $scope.disciplines.query();
-  }
+    $scope.disciplines.parseParams();
+    $scope.disciplines.query();
+  }
 ]);
