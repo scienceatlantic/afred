ae00f1850f1695a08fc5f5e06a831cc051889c37
Prasad Rajandran <prasadrajandran@gmail.com>
2016-05-27 11:16:42 -0300

    Update email footer

--- api/resources/views/layouts/emails/master.blade.php
+++ api/resources/views/layouts/emails/master.blade.php
@@ -7,9 +7,9 @@
 
 
 --------------------------------------------------------------------------------
-This email, its contents and any attached files are privileged and confidential,
-and intended for the sole use of the individual(s) or entity to whom they are
-addressed.  Any distribution, use or copying by any means of this information by
-other than the intended addressee(s) is strictly prohibited.  If you have
-received this email in error, please notify the original sender by return email
-or telephone and immediately delete this email and all attachments.
+This email, its contents and any attached files are confidential, and intended
+for the sole use of the individual(s) or entity to whom they are addressed. Any
+distribution, use or copying by any means of this information by other than the
+intended addressee(s) is strictly prohibited. If you have received this email in
+error, please notify the original sender by return email or telephone and
+immediately delete this email and all attachments.
