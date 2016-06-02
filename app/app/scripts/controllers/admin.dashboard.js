fcad5304cf3b17059337e7f515d09ac02262df47
Prasad Rajandran <prasadrajandran@gmail.com>
2016-06-02 11:42:34 -0300

    Update admin.dashboard.js

--- app/app/scripts/controllers/admin.dashboard.js
+++ app/app/scripts/controllers/admin.dashboard.js
@@ -2,40 +2,29 @@
 
 angular.module('afredApp').controller('AdminDashboardController',
   ['$scope',
-   'facilityRepositoryResource',
-   'facilityResource',
+   '$http',
   function($scope,
-           facilityRepositoryResource,
-           facilityResource) {
-    /* ---------------------------------------------------------------------
-     * Functions.
-     * --------------------------------------------------------------------- */
-    
-    $scope.getFacilityTotals = function(state) {
-      if (state == 'PUBLISHED') {
-        return facilityResource.query({
-            itemsPerPage: 1,
-            page: 1
-        });        
-      } else {
-        return facilityRepositoryResource.query({
-            itemsPerPage: 1,
-            page: 1,
-            state: state
-        });         
-      }
-    };
-    
+           $http) {
     /* ---------------------------------------------------------------------
      * Initialisation code.
      * --------------------------------------------------------------------- */
     
-    $scope.facilityStats = {
-      published: $scope.getFacilityTotals('PUBLISHED'),
-      pendingApproval: $scope.getFacilityTotals('PENDING_APPROVAL'),
-      pendingEditApproval: $scope.getFacilityTotals('PENDING_EDIT_APPROVAL'),
-      rejected: $scope.getFacilityTotals('REJECTED'),
-      rejectedEdit: $scope.getFacilityTotals('REJECTED_EDIT'),
+    // Get the data.
+    $scope.stats = $http.get($scope._config.api.address + '/dashboard');
+    $scope.stats.then(function(response) {
+      $scope.stats.data = response.data;
+      $scope.loading.stats = false;
+    }, function(response) {
+      $scope._httpError(response);
+    });
+    
+    /**
+     * AJAX loading flags.
+     *
+     * @type {object}
+     */
+    $scope.loading = {
+      stats: true
     };
   }
 ]);
