bd46b58e80df3cf93e1899c5909e1dcdd4c7bcbd
Prasad Rajandran <prasadrajandran@gmail.com>
2016-06-08 15:13:57 -0300

    Update main controller
    - Add navbar toggle for smaller screens

--- app/app/scripts/controllers/main.js
+++ app/app/scripts/controllers/main.js
@@ -14,6 +14,29 @@ angular.module('afredApp').controller('MainController',
      */
     $scope.main = {
       /**
+       * Navbar releated properties/methods.
+       */
+      navbar: {
+        /**
+         * The navbar should call this function (onload and only once) to make
+         * sure that on smaller screens expanding the hamburger menu and
+         * clicking on an item will collapse the menu after the user is
+         * redirected.
+         *
+         * The navbar has to call this function because the DOM (ng-include)
+         * might not have loaded in time for this event to attach itself to the
+         * right element.
+         * 
+         * Credit: https://github.com/twbs/bootstrap/issues/12852#issuecomment-36163121
+         */
+        toggle: function() {
+          angular.element(".navbar-nav li a").click(function(event) {
+            angular.element(".navbar-collapse").collapse('hide');
+          });
+        }
+      },
+      
+      /**
        * Footer related properties/methods.
        */
       footer: {
