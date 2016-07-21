fa0d5775bbb0778456241f51b67cc750b525cc82
Prasad Rajandran <prasadrajandran@gmail.com>
2016-07-21 16:40:15 -0300

    Update afStrToDate filter
    - Add more comments
    - Make "day" and "seconds" optional

--- app/app/scripts/filters/af-str-to-date.js
+++ app/app/scripts/filters/af-str-to-date.js
@@ -7,35 +7,58 @@
  * portion is optional.
  * 
  * Examples of valid strings:
+ * '2016-01'
  * '2016-01-28'
- * '2016-01-28 12:15:30'
+ * '2016-01-28 13:15'
+ * '2016-01-28 16:15:30'
  * 
  * @see https://docs.angularjs.org/api/ng/filter/filter
  * @see https://docs.angularjs.org/guide/filter
  * @see https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Date
  */
 
-// Input format: YYYY-MM-DD HH:MM:SS (Note: that time portion is optional).
 angular.module('afredApp').filter('afStrToDate',
   function() {
-    return function(dateString) {
+    return function(datetimeString, returnParsedStr) {
       // If falsy, return empty string.
-      if (!dateString) {
+      if (!datetimeString) {
         return '';  
       }
       
-      var dt = dateString.split(' ');
+      // Parse the string.
+      // dt[0] = date, dt[1] = time
+      var dt = datetimeString.split(' ');
+      // d[0] = YYYY, d[1] = MM, d[2] = DD
       var d = dt[0].split('-');
-      var t = dt.length > 1 ? dt[1].split(':') : null;
+      // t[0] = HH, t[1] = MM, t[2] = SS
+      var t = dt.length > 1 ? dt[1].split(':') : [];
+
+      // If string is invalid, return empty string (i.e year and month not 
+      // provided).
+      if (d.length < 2) {
+        return '';
+      }
       
       // Months start from 0 - 11 for the Date() constructor in JavaScript.
       d[1] = d[1] - 1;
       
-      if (d && t) {
+      // Full datetime.
+      if (d.length == 3 && t.length == 3) {
         return new Date(d[0], d[1], d[2], t[0], t[1], t[2]);  
-      } else {
+      }
+
+      // Datetime (without seconds). 
+      if (d.length == 3 && t.length == 2) {
+        return new Date(d[0], d[1], d[2], t[0], t[1]);
+      } 
+      
+      // Full date
+      if (d.length == 3) {
         return new Date(d[0], d[1], d[2]);
       }
+
+      // Date (year and month only).
+      return new Date(d[0], d[1]);
     };
   }
 );
