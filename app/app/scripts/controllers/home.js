021b5a6af4b06a4696e823412f9045735d5b5edd
Prasad Rajandran <prasadrajandran@gmail.com>
2016-10-07 09:20:08 -0300

    Update home
    - Check that `twttr` (twitter) exists first

--- app/app/scripts/controllers/home.js
+++ app/app/scripts/controllers/home.js
@@ -10,20 +10,38 @@
 
 angular.module('afredApp').controller('HomeController',
   ['$scope',
+   '$interval',
+   '$timeout',
    'homeResource',
    'wpResource',
   function($scope,
+           $interval,
+           $timeout,
            homeResource,
            wpResource) {
     /* ---------------------------------------------------------------------
      * Functions/Objects.
      * --------------------------------------------------------------------- */
+    /**
+     * Search properties/functions.
+     * 
+     * @type {object}
+     */
     $scope.search = {
       q: null,
 
       run: function() {
         $scope._state.go('search.q', { q: $scope.search.q });
       }
+    };
+
+    /**
+     * Loading flags.
+     * 
+     * @type {object}
+     */
+    $scope.loading = {
+      twitter: true
     };
 
     /* ---------------------------------------------------------------------
@@ -36,15 +54,49 @@ angular.module('afredApp').controller('HomeController',
     // Get "Have a Look" content.
     $scope.haveALook = homeResource.get();
 
-    // Load Twitter timeline and hide any images.
+    // Get "Recent Tweets" content.
+    // Load Twitter timeline and hide any images (check that it exists first). 
     // @see https://dev.twitter.com/web/javascript/initialization
     // @see https://dev.twitter.com/web/javascript/events
-    twttr.widgets.load();
-    twttr.events.bind('loaded', function(event) {
-      event.widgets.forEach(function(widget) {
-        angular.element('#' + widget.id).contents()
-          .find('.timeline-Tweet-media').hide();
-      });
-    });
+    var attemptCount = 1;
+    var intervalId = $interval(function() {
+      if (attemptCount++ <= 15) {
+        if (typeof twttr !== 'undefined' && twttr !== null) {
+          // Cancel interval.
+          $interval.cancel(intervalId);
+
+          // Load twitter widget.
+          twttr.widgets.load();
+
+          // Hide images and readjust height (after removing images the height
+          // needs to be readjusted to match the new height of the content).
+          twttr.events.bind('loaded', function(event) {
+            event.widgets.forEach(function(widget) {
+              var id = '#' + widget.id;
+
+              // Hide images.
+              angular.element(id).contents().find('.timeline-Tweet-media')
+                .hide();
+
+              // Readjust height.
+              var contentHeight = angular.element(id).contents()
+                .find('.timeline-Widget').height();
+              angular.element(id).height(contentHeight);
+            });
+
+            // Set loading flag to false (after an artificial delay).
+            $timeout(function() {
+              $scope.loading.twitter = false;
+            }, 600);
+          });
+        }
+      } else {
+        // Maximum number of attempts reached, quit.
+        $interval.cancel(intervalId);
+        
+        // Set loading flag to false.
+        $scope.loading.twitter = false;
+      }
+    }, 50);
   }
 ]);
