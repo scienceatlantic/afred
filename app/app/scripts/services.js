e6cde34fa87c57fa01606a741da5ab6e2f247cf4
Prasad Rajandran <prasadrajandran@gmail.com>
2016-10-30 16:04:08 -0300

    Add `Repository` service

--- app/app/scripts/services.js
+++ app/app/scripts/services.js
@@ -87,6 +87,11 @@ angular.module('afredApp').service('algolia', [
   }
 ]);
 
+/* ---------------------------------------------------------------------
+ * JsDiff.
+ * @see https://github.com/kpdecker/jsdiff
+ * --------------------------------------------------------------------- */
+
 angular.module('afredApp').service('JsDiff', [
   function() {
     function formatter(original, edit, isHtml, diffType) {
@@ -145,6 +150,138 @@ angular.module('afredApp').service('JsDiff', [
 
     this.lines = function(original, edit, isHtml) {
       return formatter(original, edit, isHtml, 'lines');
+    };
+  }
+]);
+
+
+/* ---------------------------------------------------------------------
+ * Facility Repository service.
+ * TODO: comments..
+ * --------------------------------------------------------------------- */
+
+angular.module('afredApp').service('Repository', [
+  '$q',
+  '$rootScope',
+  'disciplineResource',
+  'facilityRepositoryResource',
+  'organizationResource',
+  'provinceResource',
+  'sectorResource',
+  function($q,
+           $rootScope,
+           disciplineResource,
+           facilityRepositoryResource,
+           organizationResource,
+           provinceResource,
+           sectorResource) {
+
+    this.get = function(id) {
+      return facilityRepositoryResource.get({
+        facilityRepositoryId: id ? id : 0
+      }, function() {
+        // Do nothing if successful.
+      }, function(response) {
+        $rootScope._httpError403(response);
+      });
+    };
+
+    this.getFacility = function(fr) {
+      var data = angular.copy(fr.data);
+      var facility = data.facility;
+      facility.organization = data.organization;
+      facility.contacts = data.contacts;
+      facility.equipment = data.equipment;
+      facility.state = fr.state;  
+      
+      try {
+        facility.isPublic = fr.publishedFacility.isPublic;
+      } catch (e) {
+        // Do nothing if it fails.
+      }
+            
+      // Primary contact & contacts section. In the DB primary contacts and
+      // regular contacts are stored in separate tables, however, when the user
+      // is viewing it, it's stored in a single array (where the first element
+      // is the primary contact).
+      if (!data.contacts || !angular.isArray(data.contacts)) {
+        facility.contacts = [];
+        facility.contacts.push(data.primaryContact);
+      } else {
+        facility.contacts.unshift(data.primaryContact);
+      }
+      
+      // Organization section. Check if the facility belongs to an existing
+      // organization or a new organization. If it belongs to an existing
+      // organization, grab the details from the API.
+      if (data.facility.organizationId) {
+        facility.organization = organizationResource.get({
+          organizationId: data.facility.organizationId
+        }, function() {
+          // Do nothing if successful.
+        }, function(response) {
+          $rootScope._httpError(response);
+        });
+      }
+      
+      // Province section.
+      facility.province = provinceResource.get({
+        provinceId: data.facility.provinceId
+      }, function() {
+        // Do nothing if successful.
+      }, function(response) {
+        $rootScope._httpError(response);
+      });
+      
+      // Disciplines section. Grab the complete list of disciplines from the API
+      // so that we can get the names (the facility repository record only
+      // contains the IDs of the disciplines).
+      facility.disciplines = [];
+      var isDisciplineReady = $q.defer();
+      var disciplines = disciplineResource.queryNoPaginate(function() {
+        angular.forEach(disciplines, function(d) {
+          if (data.disciplines.indexOf(d.id) >= 0) {
+            facility.disciplines.push(d);
+          }
+        });
+        isDisciplineReady.resolve();
+      }, function(response) {
+        $rootScope._httpError(response); 
+      });
+      
+      // Sectors section. (Same as disciplines).
+      facility.sectors = [];
+      var isSectorReady = $q.defer();
+      var sectors = sectorResource.queryNoPaginate(function() {
+        angular.forEach(sectors, function(s) {
+          if (data.sectors.indexOf(s.id) >= 0) {
+            facility.sectors.push(s);
+          }
+        });
+        isSectorReady.resolve();
+      }, function(response) {
+        $rootScope._httpError(response); 
+      });
+
+      // Set up promise.
+      var deferred = $q.defer();
+      facility.$promise = deferred.promise;
+      facility.$promise.then(function() {
+        facility.$resolved = true;
+      });
+
+      // Waits for all the async calls to complete before resolving the
+      // promise.
+      $q.all([
+        facility.organization.$promise,
+        facility.province.$promise,
+        isDisciplineReady.promise,
+        isSectorReady.promise
+      ]).then(function() {
+        deferred.resolve(facility);
+      });
+
+      return facility;
     };
   }
 ]);
