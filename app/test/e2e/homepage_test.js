efba72a87f69a8ad053f117c9dffe166357437d8
Prasad Rajandran <prasadrajandran@gmail.com>
2017-02-12 22:01:02 -0400

    Add additional test to homepage

--- app/test/e2e/homepage_test.js
+++ app/test/e2e/homepage_test.js
@@ -1,7 +1,46 @@
 describe('AFRED homepage', function() {
+  var EC = protractor.ExpectedConditions;
+
   it('should see title', function() {
     browser.get(browser.baseUrl + '/');
 
     expect(browser.getTitle()).toEqual('Home | AFRED');
   });
+
+  it('should try to search for something and redirect to the search page', 
+    function() {
+      var query = 'microscopes';         
+
+      element(by.model('search.q')).sendKeys(query);
+
+      $('button[type="submit"]').click();
+
+      expect(browser.getTitle()).toEqual('Search | AFRED');
+
+      $('p[data-ng-if="search.results.length"], ' + 
+        'p[data-ng-if="!search.results.length"]').getText()
+        .then(function(text) {
+          var results = '';
+          var total = parseInt(text.replace(/^\D+/g, ''));
+          
+          if (total == 1) {
+            results = EC.textToBePresentInElement(
+              $('p[data-ng-if="search.results.length"]'),
+              'We found a total of ' + total + ' piece of equipment for \'' +
+              query + '\'');
+          } else if (total > 1) {
+            results = EC.textToBePresentInElement(
+              $('p[data-ng-if="search.results.length"]'),
+              'We found a total of ' + total + ' pieces of equipment for \'' +
+              query + '\'');
+          } else {
+            results = EC.textToBePresentInElement(
+              $('p[data-ng-if="!search.results.length"]'), 
+              'Sorry, we couldn\'t find anything for \'' + query + '\'');
+          }
+
+          browser.wait(results, 5000);          
+        });      
+    }
+  );
 });
