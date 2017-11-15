f9129cf2b8b5bebc675ec70c86f6f24597729215
Prasad Rajandran <prasadrajandran@gmail.com>
2017-11-15 10:31:36 -0400

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('/forms', 'FormController');
+
+Route::post('/entities', 'EntityController@action');
+Route::put('/entities', 'EntityController@action');
+Route::resource('/entities', 'EntityController', ['only' => [
+    'index', 'show'
+]]);
