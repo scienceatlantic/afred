031d15d565a165522316de9022664040ec7e4512
Prasad Rajandran <prasadrajandran@gmail.com>
2018-03-10 22:06:08 -0400

    Add ILO to email template

--- resources/views/emails/afred/form-entries/facilities/author/published.blade.php
+++ resources/views/emails/afred/form-entries/facilities/author/published.blade.php
@@ -23,15 +23,15 @@ wish to make edits to your listing at any time, please follow the instructions
 on this page:<br>
 [https://localhost/afred-wp-demo/update-facility](https://localhost/afred-wp-demo/update-facility)
 
-{{--  @if ($SOMETHING)
+@if ($formEntry->ilo)
 A copy of this email has also been sent to your organization's industry liaison
-officer (ILO), {!! $iloName !!}, at {!! $iloEmail !!}.
-@endif  --}}
+officer (ILO), {!! $formEntry->ilo->name !!}, at
+<a href="mailto:{!! $formEntry->ilo->email !!}">{!! $formEntry->ilo->email !!}</a>.
+@endif
 
 If you have any questions or comments, please contact Lois Whitehead, Science
 Atlantic's Executive Director, at
-<a href="mailto:lois.whitehead@scienceatlantic.ca">lois.whitehead@scienceatlantic.ca</a>
-[lois.whitehead@scienceatlantic.ca](lois.whitehead@scienceatlantic.ca).
+<a href="mailto:lois.whitehead@scienceatlantic.ca">lois.whitehead@scienceatlantic.ca</a>.
 
 Regards,<br>
 AFRED Bot
