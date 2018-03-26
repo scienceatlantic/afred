d766cdde67a15a81649997021ae8f56a83798acb
Prasad Rajandran <prasadrajandran@gmail.com>
2018-03-25 22:42:40 -0400

    Update

--- database/seeds/DirectoriesTableSeeder.php
+++ database/seeds/DirectoriesTableSeeder.php
@@ -23,10 +23,10 @@ class DirectoriesTableSeeder extends BaseSeeder
                 'name'              => 'University of Calgary',
                 'shortname'         => 'ucalgary',
                 'resource_folder'   => 'ucalgary',
-                'wp_base_url'       => 'http://localhost/afred-wp-demo',
-                'wp_admin_base_url' => 'http://localhost/afred-wp-demo/wp-admin',
-                'wp_api_base_url'   => 'http://localhost/afred-wp-demo/wp-json/wp/v2',
-                'wp_api_password'   => 'cm9vdDp2VFptIG1LZEMgSFpDNSBNV2JiIHBlS3MgU2RSVQ==', // TODO: Commit only dummy credentials to GitHub!
+                'wp_base_url'       => 'http://localhost/afred-wp-demo2',
+                'wp_admin_base_url' => 'http://localhost/afred-wp-demo2/wp-admin',
+                'wp_api_base_url'   => 'http://localhost/afred-wp-demo2/wp-json/wp/v2',
+                'wp_api_password'   => 'YWZyZWR3cGRlbW8yOnlIQTUgVlRBSSBqU3V4IGVXdkogUE8zNCBIZ1Aw', // TODO: Commit only dummy credentials to GitHub!
             ],            
         ];
 
