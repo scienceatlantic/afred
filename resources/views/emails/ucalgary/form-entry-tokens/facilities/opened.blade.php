a1a77107999db4d74c0304f2869a23c90ee9477a
Prasad Rajandran <prasadrajandran@gmail.com>
2018-03-27 23:25:57 -0400

    Bug fix: escape URLs

--- resources/views/emails/ucalgary/form-entry-tokens/facilities/opened.blade.php
+++ resources/views/emails/ucalgary/form-entry-tokens/facilities/opened.blade.php
@@ -5,7 +5,7 @@ You have requested to update {!! $formEntry->data['pagination_title'] !!}.
 
 Click on the following URL to access your facility for editing. If you are
 unable to click on the URL, copy and paste it into your browser.
-<a href="{{ $token->wp_edit_url }}" target="_blank">{{ $token->wp_edit_url }}</a>
+<a href="{{ $token->wp_edit_url }}" target="_blank">{!! $token->wp_edit_url !!}</a>
 
 Regards,<br>
 uCalgary Bot
