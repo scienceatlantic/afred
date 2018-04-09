9c75cbd4f5ac5ba09a179cbcfd5382885ba237e2
Prasad Rajandran <prasadrajandran@gmail.com>
2018-04-09 00:38:46 -0400

    Update

--- database/seeds/AfredFormDataSeeder.php
+++ database/seeds/AfredFormDataSeeder.php
@@ -29,6 +29,17 @@ class AfredFormDataSeeder extends BaseFormSeeder
      */
     public function run()
     {
+        $directories = [[
+            'name'              => 'Atlantic Facilities and Research Equipment Database',
+            'shortname'         => 'AFRED',
+            'resource_folder'   => 'afred',
+            'wp_base_url'       => 'http://localhost/afred-wp-demo',
+            'wp_admin_base_url' => 'http://localhost/afred-wp-demo/wp-admin',
+            'wp_api_base_url'   => 'http://localhost/afred-wp-demo/wp-json/wp/v2',
+            'wp_api_password'   => 'cm9vdDp2VFptIG1LZEMgSFpDNSBNV2JiIHBlS3MgU2RSVQ=='
+        ]];
+        BaseSeeder::saveModels('Directory', $directories);
+
         $directory = Directory::findDirectory('Atlantic Facilities and Research Equipment Database');
 
         $languageCode = LanguageCode::findCode('en');
