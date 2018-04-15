0a8883d8c9694756f111560f96d0ea0f350da3f2
Prasad Rajandran <prasadrajandran@gmail.com>
2018-04-14 23:05:37 -0400

    Add form submission message templates

--- app/Form.php
+++ app/Form.php
@@ -3,9 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use View;
 
 class Form extends Model
 {
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'submission_messages'
+    ];
+
     public function directory()
     {
         return $this->belongsTo('App\Directory');
@@ -45,5 +55,42 @@ class Form extends Model
     public function searchSections()
     {
         return $this->hasManyThrough('App\SearchSection', 'App\FormSection');
+    }
+
+    public function getSubmissionMessagesAttribute()
+    {
+        $form = $this->fresh();
+
+        $template = 'wp.'
+            . $form->directory->resource_folder
+            . '.forms.'
+            . $form->resource_folder
+            . '.';
+
+        // Can't pass `$form` itself, because that will cause an internal 
+        // infinite loop because each instance is trying to retrieve this
+        // attribute (i.e. $form->submission_messages).
+        $data = [
+            'directory' => $form->directory
+        ];
+
+        return [
+            'successful' => View::make(
+                $template . 'submission-successful',
+                $data
+            )->render(),
+            'successful_edit' => View::make(
+                $template . 'submission-edit-successful',
+                $data
+            )->render(),
+            'failed' => View::make(
+                $template . 'submission-failed',
+                $data
+            )->render(),
+            'failed_edit' => View::make(
+                $template . 'submission-edit-failed',
+                $data
+            )->render(),    
+        ];
     }
 }
---
+++ resources/views/wp/afred/forms/facilities/submission-edit-failed.blade.php
@@ -0,0 +1,9 @@
+<p>
+  Sorry! A technical difficulty was encountered and your information was not
+  successfully submitted.
+</p>
+
+<p>
+  Please contact
+  <a href="mailto:afred@scienceatlantic.ca">afred@scienceatlantic.ca</a>.
+</p>
